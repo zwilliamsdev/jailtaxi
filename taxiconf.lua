e51ff114442a0c162660a6a4af42a784fe61b3d2
Zachary Williams <zwilliams.dev@gmail.com>
2020-01-23 19:57:39 -0500

    Create config file

---
+++ taxiconf.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.baseFare = 75 -- Base price of taxi ride
+Config.pricePerMile = 15 -- Price per mile of driving
+
+-- [[ Locations ]] --
+Config.taxiMarker = vector3(1853.115, 2582.203, 45.66803)
